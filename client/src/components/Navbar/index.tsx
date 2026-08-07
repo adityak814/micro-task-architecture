c27b862395d684b0d489f9b79925c3ef6e1e8fb1
adityak814 <aditya.kumaraug814@gmail.com>
2026-08-08 04:13:17 +0530

    removed search from navbar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu, Search, Settings, Sun, Moon, User } from "lucide-react";
+import { Menu, Settings, Sun, Moon, User } from "lucide-react";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed, setIsDarkMode } from "@/state";
 import Link from "next/link";
@@ -29,7 +29,6 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
-      {/* Search Bar */}
       <div className="flex items-center gap-8">
         {!isSidebarCollapsed ? null : (
           <button
@@ -38,14 +37,6 @@ const Navbar = () => {
             <Menu className="h-8 w-8 dark:text-white" />
           </button>
         )}
-        <div className="relative flex h-min w-[200px]">
-          <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
-          <input
-            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
-            type="search"
-            placeholder="Search..."
-          />
-        </div>
       </div>
       {/* Icons */}
       <div className="flex items-center">
